b05acdae40008be5890f9ccf5ff0b30e64fa8894
AmruthashreeAM <>
2020-09-24 15:28:34 -0500

    Uncommented the functions for team to work on services

--- app/src/main/java/com/se1/team3/campuscarrental/db/DBHandler.java
+++ app/src/main/java/com/se1/team3/campuscarrental/db/DBHandler.java
@@ -5,9 +5,9 @@ import com.se1.team3.campuscarrental.models.SystemUser;
 public interface DBHandler {
 //    TODO - Amrutha - uncomment these and implement
 
-//    public SystemUser login(String username, String password);
-//
-//    public void signUpUser(SystemUser systemUser);
+   public SystemUser login(String username, String password);
+
+    public void signUpUser(SystemUser systemUser);
 
 //    other functions
 }
