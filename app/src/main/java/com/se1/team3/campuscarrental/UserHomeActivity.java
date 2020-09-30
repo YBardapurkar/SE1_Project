d205612c9489ae294d73b0962df1348dc67b19e3
Shubham Shah <shahshubham845@gmail.com>
2020-09-29 19:08:04 -0500

    Updated User Home activity.

--- app/src/main/java/com/se1/team3/campuscarrental/UserHomeActivity.java
+++ app/src/main/java/com/se1/team3/campuscarrental/UserHomeActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -15,9 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class UserHomeActivity extends AppCompatActivity {
 
 //    -------- USER HOME PAGE --------------- [Shubham Shah]
-    Button btn_profile, btn_updateProfile, btn_searchCar, btn_carDetails, btn_requestRental,
-            btn_viewAllReserves, btn_cancelReserve, btn_viewCurrReserve, btn_logout;
+    Button btn_profile,  btn_searchCar, btn_viewAllReserves, btn_logout;
     SharedPreferences sharedPreferences;
+    TextView textWelcome;
 
     static final String PREFERENCES = "SharedPreferences";
     static final String USERNAME = "username";
@@ -29,81 +30,28 @@ public class UserHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
         setTitle(R.string.title_user_home);
 
-        btn_profile = findViewById(R.id.view_profile);
-        btn_updateProfile = findViewById(R.id.update_profile);
-        btn_searchCar = findViewById(R.id.search_car);
-        btn_carDetails = findViewById(R.id.car_details);
-        btn_requestRental = findViewById(R.id.req_rental);
-        btn_viewAllReserves = findViewById(R.id.view_reserves);
-        btn_cancelReserve = findViewById(R.id.cancel_reserve);
-        btn_viewCurrReserve = findViewById(R.id.selec_reserve);
-        btn_logout = findViewById(R.id.logout);
-
-
-        btn_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "View Profile", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_updateProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Update Profile", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_searchCar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Search Car", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_carDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Car Details", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_requestRental.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Request Rental", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_viewAllReserves.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "View All Reservations", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_viewCurrReserve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Cancel Reservation", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_cancelReserve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Current Reservation", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UserHomeActivity.this, "Logout", Toast.LENGTH_SHORT).show();
-                logout();
-            }
-        });
         sharedPreferences =  getApplicationContext().getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        String welcomeMessage = String.format(getResources().getString(R.string.welcome_message)+ " %1$s", sharedPreferences.getString(USERNAME, ""));
+//        String welcomeMessage = "Shubham Shah";
+        textWelcome = findViewById(R.id.text_user_welcome);
+        textWelcome.setText(welcomeMessage);
+
+        btn_profile = findViewById(R.id.btn_profile);
+        btn_searchCar = findViewById(R.id.btn_search_car);
+        btn_viewAllReserves = findViewById(R.id.btn_reserves);
+        btn_logout = findViewById(R.id.btn_logout);
+
+        btn_profile.setOnClickListener(v -> Toast.makeText(UserHomeActivity.this, "### View Profile", Toast.LENGTH_SHORT).show());
+
+        btn_searchCar.setOnClickListener(v -> Toast.makeText(UserHomeActivity.this, "!!! Search Car", Toast.LENGTH_SHORT).show());
+
+
+        btn_viewAllReserves.setOnClickListener(v -> Toast.makeText(UserHomeActivity.this, " $$$ View All Reservations", Toast.LENGTH_SHORT).show());
+
+        btn_logout.setOnClickListener(v -> {
+            Toast.makeText(UserHomeActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+            logout();
+        });
     }
 
     public void logout() {
