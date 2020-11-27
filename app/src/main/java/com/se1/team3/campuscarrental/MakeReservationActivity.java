d0e3e43d26390d7a85ce3638d6ed7ae17995ee9f
AmruthashreeAM <>
2020-11-27 13:19:32 -0600

    Added implementation for Make reservation user page

--- app/src/main/java/com/se1/team3/campuscarrental/MakeReservationActivity.java
+++ app/src/main/java/com/se1/team3/campuscarrental/MakeReservationActivity.java
@@ -57,7 +57,7 @@ public class MakeReservationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_car_details);
+        setContentView(R.layout.activity_make_reservation);
 
         setTitle("Car Details");
 
